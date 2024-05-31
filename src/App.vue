05c246365288f84f13f11a0042d39b2a49ed97b5
davide-piarulli <davide.piarulli.1@gmail.com>
2024-05-31 14:55:01 +0100

    [add] Milestone 4, print in console

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,40 @@
 <script>
-import axios from 'axios';
-export default {};
+import axios from "axios";
+import { store } from "./assets/data/store";
+
+export default {
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  methods: {
+    getApi() {
+      // console.log("ciao");
+      axios
+        .get(store.apiUrl)
+        .then((result) => {
+          this.projects = result.data.data;
+          console.log(result.data.data);
+        })
+        .catch((error) => {
+          console.log(error.message);
+          error = error.message;
+        });
+    },
+  },
+  mounted() {
+    this.getApi();
+  },
+};
 </script>
 
 <template>
   <div>
     <h1>Progetti</h1>
+    <ul>
+      <li></li>
+    </ul>
   </div>
 </template>
 
